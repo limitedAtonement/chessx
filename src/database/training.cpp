7e7dbdb6620e09fd921666d8216ac2d0de6593af
lmat <dartme18@gmail.com>
2024-03-29 10:32:24 -0400

    Training continues to second line

--- src/database/training.cpp
+++ src/database/training.cpp
@@ -7,6 +7,8 @@
 #include <iostream>
 
 static constexpr char const * datetime_format = "yyyy-MM-ddThh:mm:ss";
+static const QString starting_game {R"(1. e4 e5 2. Nf3 Nc6 ( d5 3. Nc3 ) 3. Bb5 *)"};
+static const QString starting_game_one {R"(1. e4 e5 2. Nf3 Nc6 ( d5 3. Nc3{first reviewed: 2023-01-01T00:00:00; last reviewed: 2024-01-01T00:00:00, next review: 2024-01-02T00:00:00}) 3. Bb5{first reviewed: 2023-01-01T00:00:00; last reviewed: 2024-01-01T00:00:00, next review: 2024-01-03T00:00:00} *)"};
 
 static const QRegularExpression last_reviewed_regex("last reviewed: ([^;]+);");
 static std::time_t get_last_reviewed_from_annotation(QString const & annotation)
@@ -88,7 +90,7 @@ static training_line get_training_line(GameX & game, GameId id)
 /** Reads all child nodes in the game and adds a training_line for
  * any leaf nodes found
  */
-void get_training_lines(GameX & game, GameId id, std::vector<training_line> & lines)
+static void get_training_lines(GameX & game, GameId id, std::vector<training_line> & lines)
 {
     if (game.nextMove() == NO_MOVE)
     {
@@ -110,6 +112,7 @@ void get_training_lines(GameX & game, GameId id, std::vector<training_line> & li
 
 void Training::initialize(Database & db, Color color)
 {
+    lines.clear();
     for (quint64 game_id{0}; game_id < db.count(); ++game_id)
     {
         games.emplace_back();
@@ -119,13 +122,13 @@ void Training::initialize(Database & db, Color color)
             continue;
         }
         games.back().moveToStart();
-        std::cout << "     initialized with game " << game_id << " has " << games.back().cursor().countMoves() << " moves.\n";
         get_training_lines(games.back(), game_id, lines);
     }
     std::sort(lines.begin(), lines.end());
     // If the trainee is White, it's his turn.
     // If the trainee is Black, the trainer has already made a move for white.
     current_move_in_line = color == White ? 0 : 1;
+    missed_any_this_time = false;
     this->training_color = color;
 }
 
@@ -187,6 +190,7 @@ std::optional<GameId> Training::get_game_id(void) const
     }
     return lines.front().game_id;
 }
+
 GameX * Training::get_game(void)
 {
     if (lines.empty())
@@ -212,15 +216,25 @@ bool Training::handle_done()
         return false;
     }
     line.last_reviewed = std::time(nullptr);
-    int increment_sign {missed_any_this_time ? -1 : 1};
-    std::time_t increment {line.next_review - line.last_reviewed};
+    int const increment_sign {missed_any_this_time ? -1 : 1};
+    std::time_t increment {(line.next_review - line.last_reviewed)};
+    if (increment == 0)
+        // Start with 20 second review period
+        increment = 20;
     increment *= increment_sign;
     std::time(&line.last_reviewed);
+    if (!line.first_reviewed)
+    {
+        line.first_reviewed = line.last_reviewed;
+    }
+    QDateTime const first_reviewed = QDateTime::fromSecsSinceEpoch(line.first_reviewed);
     line.next_review += increment;
-    QDateTime last_reviewed = QDateTime::fromSecsSinceEpoch(line.last_reviewed);
-    QDateTime next_review = QDateTime::fromSecsSinceEpoch(line.last_reviewed);
-    QString new_annotation = QString{"last reviewed: %1; next review: %2"}
-        .arg(last_reviewed.toString(datetime_format)).arg(next_review.toString(datetime_format));
+    QDateTime const last_reviewed = QDateTime::fromSecsSinceEpoch(line.last_reviewed);
+    QDateTime const next_review = QDateTime::fromSecsSinceEpoch(line.last_reviewed);
+    QString new_annotation = QString{"first reviewed: %1; last reviewed: %2; next review: %3"}
+        .arg(first_reviewed.toString(datetime_format))
+        .arg(last_reviewed.toString(datetime_format))
+        .arg(next_review.toString(datetime_format));
     line.game->dbSetAnnotation(new_annotation, line.leaf_id, GameX::Position::AfterMove);
     return true;
 }
@@ -277,7 +291,7 @@ Training::Training(unsigned n)
 #include "settings.h"
 #include "chessxsettings.h"
 
-void test_successful_review(Database & db)
+static void test_successful_review(MemoryDatabase & db)
 {
     std::vector<Move> moves{
         {chessx::Square::e2, chessx::Square::e4},
@@ -319,7 +333,7 @@ void test_successful_review(Database & db)
     std::cout << "test complete\n";
 }
 
-void test_failed_review(Database & db)
+static void test_failed_review(MemoryDatabase & db)
 {
     std::vector<Move> moves{
         {chessx::Square::e2, chessx::Square::e4},
@@ -370,7 +384,7 @@ void test_failed_review(Database & db)
     std::cout << "test complete\n";
 }
 
-void train_one_per_day(Database & db)
+static void train_one_per_day(MemoryDatabase & db)
 {
     std::vector<Move> moves{
         {chessx::Square::e2, chessx::Square::e4},
@@ -407,18 +421,99 @@ void train_one_per_day(Database & db)
     std::cout << "test complete\n";
 }
 
+static void progress_to_next_training(MemoryDatabase & db)
+{
+    std::cout << "progress_to_next_training...\n";
+    if (!db.openString(starting_game_one))
+    {
+        std::cerr << "Failed to open test training file\n";
+        return;
+    }
+    if (!static_cast<Database&>(db).parseFile())
+    {
+        std::cerr << "Failed to parse game\n";
+        return;
+    }
+    std::vector<Move> moves{
+        {chessx::Square::e2, chessx::Square::e4},
+        {chessx::Square::g1, chessx::Square::f3},
+        {chessx::Square::f1, chessx::Square::b5},
+    };
+    Training t {0};
+    t.initialize(db, White);
+    unsigned move_number{0};
+    for (Move m : moves)
+    {
+        if (move_number++ == 2 && t.last_response().from() != chessx::Square::b8)
+        {
+            std::cerr << "  Expected b8 move now\n";
+        }
+        if (t.last_response().from() == chessx::Square::d7)
+        {
+            m = {chessx::Square::b1, chessx::Square::c3};
+        }
+        if (!t.move(m))
+        {
+            std::cerr << "   -Failure on  move " << m.toAlgebraicDebug().toStdString() << "\n";
+            break;
+        }
+    }
+    if (!t.finished_current_training())
+    {
+        std::cerr << "   Training should be finished\n";
+        return;
+    }
+    if (t.done_training())
+    {
+        std::cerr << "   Training should not be done yet\n";
+        return;
+    }
+    GameX * game{t.get_game()};
+    if (!game)
+    {
+        std::cerr << "   Couldn't get training game\n";
+        return;
+    }
+    std::optional<GameId> game_id {t.get_game_id()};
+    if (!game_id)
+    {
+        std::cerr << "   Couldn't get game id\n";
+        return;
+    }
+    db.replace(*game_id, *game);
+    //std::cout << "   finished with first line (nc6); looking for d5...\n";
+    t.initialize(db, White);
+    move_number = 0;
+    for (Move m : moves)
+    {
+        if (move_number++ == 2 && t.last_response().from() != chessx::Square::d7)
+        {
+            std::cerr << "  Error Expected d7 move now\n";
+        }
+        if (t.last_response().from() == chessx::Square::d7)
+        {
+            m = {chessx::Square::b1, chessx::Square::c3};
+        }
+        if (!t.move(m))
+        {
+            std::cerr << "   -Failure on  move " << m.toAlgebraicDebug().toStdString() << "\n";
+            break;
+        }
+    }
+    std::cout << "test complete\n";
+}
+
 void test_training(void)
 {
     // memoryDatabase needs this global
     AppSettings = new ChessXSettings;
     AppSettings->setValue("/General/automaticECO", false);
-    char const * const db_file{"../practice.pgn"};
-    std::vector<std::function<void(Database &)>> tests {test_successful_review, test_failed_review,
-        train_one_per_day};
-    for (std::function<void(Database &)> const & test : tests)
+    std::vector<std::function<void(MemoryDatabase &)>> tests {test_successful_review, test_failed_review,
+        train_one_per_day, progress_to_next_training};
+    for (std::function<void(MemoryDatabase &)> const & test : tests)
     {
         MemoryDatabase db;
-        if (!db.open(db_file, true))
+        if (!db.openString(starting_game))
         {
             std::cerr << "Failed to open test training file\n";
             return;
